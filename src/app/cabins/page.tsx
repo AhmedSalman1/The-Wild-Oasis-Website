f62fcb4ffb00d634e6d7b36e34630d5009f088eb
Ahmed Salman <a7medsalman6@gmail.com>
2026-08-11 17:57:25 +0300

    feat(pages): add initial structure for cabins, profile, and reservations

--- src/app/cabins/page.tsx
+++ src/app/cabins/page.tsx
@@ -1,11 +1,37 @@
+import CabinCard from "@/components/cabins/CabinCard";
+
 export const metadata = {
 	title: "Cabins",
 };
 
 export default function Page() {
+	// CHANGE
+	const cabins = [];
+
 	return (
 		<div>
-			<h1>Cabins page</h1>
+			<h1 className="text-accent-400 mb-2 text-2xl font-medium md:text-3xl lg:mb-5 lg:text-4xl">
+				Our Luxury Cabins
+			</h1>
+			<p className="text-primary-200 mb-6 text-base md:text-lg lg:mb-10">
+				Cozy yet luxurious cabins, located right in the heart of the Italian
+				Dolomites.{" "}
+				<span className="hidden md:inline">
+					Imagine waking up to beautiful mountain views, spending your days
+					exploring the dark forests around, or just relaxing in your private
+					hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+					home away from home.
+				</span>{" "}
+				The perfect spot for a peaceful, calm vacation. Welcome to paradise.
+			</p>
+
+			{cabins.length > 0 && (
+				<div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:gap-12 xl:gap-14">
+					{cabins.map((cabin) => (
+						<CabinCard cabin={cabin} key={cabin.id} />
+					))}
+				</div>
+			)}
 		</div>
 	);
 }
