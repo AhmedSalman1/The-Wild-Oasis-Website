4efc897f3d513e815e9fd2244bed29295d9d713e
Ahmed Salman <a7medsalman6@gmail.com>
2026-08-12 16:32:20 +0300

    feat(supabase): set up typed Supabase client

---
+++ src/lib/supabase.ts
@@ -0,0 +1,7 @@
+import { Database } from "@/types/database.types";
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient<Database>(
+	process.env.SUPABASE_URL!,
+	process.env.SUPABASE_KEY!
+);
